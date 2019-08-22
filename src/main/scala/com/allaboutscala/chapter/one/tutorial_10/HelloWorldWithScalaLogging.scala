0be9188ac3809017d9e7bf6841f2d0de7f639c93
David Gilmore <dgilmore@spotx.tv>
2019-08-22 12:14:50 +0100

    Tutorial 10

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_10/HelloWorldWithScalaLogging.scala
@@ -0,0 +1,9 @@
+package com.allaboutscala.chapter.one.tutorial_10
+
+import com.typesafe.scalalogging.LazyLogging
+
+object HelloWorldWithScalaLogging extends App with LazyLogging {
+
+  logger.info("Hello World from Scala Logging")
+
+}
