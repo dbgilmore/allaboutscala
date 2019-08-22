3de746ceb2faded79a179a45f5336a9381546f66
David Gilmore <dgilmore@spotx.tv>
2019-08-22 12:14:35 +0100

    Tutorial 7

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_07/HelloWorldWithArgumentsDebug.scala
@@ -0,0 +1,11 @@
+package com.allaboutscala.chapter.one.tutorial_07
+
+object HelloWorldWithArgumentsDebug extends App {
+
+  println("Hello World With Arguments Scala Application!")
+
+  println("Command line arguments are: ")
+
+  println(args.mkString(", "))
+  
+}
